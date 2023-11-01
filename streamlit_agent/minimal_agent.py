0fc52837990143d6d75839b197c7174830d7b21a
spy0578 <spy0578@qq.com>
2023-11-01 11:58:46 +0800

    Update minimal_agent.py

--- streamlit_agent/minimal_agent.py
+++ streamlit_agent/minimal_agent.py
@@ -3,10 +3,8 @@ from langchain.agents import AgentType, initialize_agent, load_tools
 from langchain.callbacks import StreamlitCallbackHandler
 import streamlit as st
 
-open_api_key = "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"
 
-
-llm = OpenAI(openai_api_key=open_api_key, temperature=0, streaming=True)
+llm = OpenAI(temperature=0, streaming=True)
 tools = load_tools(["ddg-search"])
 agent = initialize_agent(
     tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
